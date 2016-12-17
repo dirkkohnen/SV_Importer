26aa69614d71cb7b59d20d40796c94f4c2f4ef0b
dirkkohnen <dkohnen@me.com>
2016-12-17 07:37:52 +0100

    Initial

---
+++ SV_Importer/src/de/scopevisio/importer/SV_Importer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.scopevisio.importer;
+
+/**
+ * @author dirk.kohnen
+ *
+ */
+public class SV_Importer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
